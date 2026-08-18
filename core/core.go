e720fabda5c24ad583d4ce7aab3d6f176c89512f
sinspired <ggmomo@gmail.com>
2026-08-18 13:57:44 +0800

    chore: bump X-ray core to v26.7.29，AllowInsecure

--- core/core.go
+++ core/core.go
@@ -20,7 +20,7 @@ import (
 var (
 	Version_x byte = 26
 	Version_y byte = 7
-	Version_z byte = 28
+	Version_z byte = 29
 )
 
 var (
--- infra/conf/transport_security.go
+++ infra/conf/transport_security.go
@@ -359,7 +359,7 @@ func (c *TLSConfig) Build() (proto.Message, error) {
 	config.MasterKeyLog = c.MasterKeyLog
 
 	if c.AllowInsecure {
-		return nil, errors.PrintRemovedFeatureError(`"allowInsecure"`, `"pinnedPeerCertSha256"(pcs) and "verifyPeerCertByName"(vcn)`)
+		config.AllowInsecure = true
 	}
 	if c.PinnedPeerCertSha256 != "" {
 		for v := range strings.SplitSeq(c.PinnedPeerCertSha256, ",") {
